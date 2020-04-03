43d8b33dfee67a78e6259774f5ba88621babea18
lazaruga <32501398+lazaruga@users.noreply.github.com>
2020-04-03 17:05:11 +0200

    Create Script

---
+++ init-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update
+sudp apt-get upgrade -y
+sudo apt-get install tasksel -y
+sudo tasksel ubuntu-desktop
+wget --output-document odoo.sh http://lazarugashops.com/odoo/odoo.sh
+sudo chmod +x odoo.sh
+sudo sh ./odoo.sh
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt update
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
